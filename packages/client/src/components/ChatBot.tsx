17d9297f57e76252b8bff5453e0d5ef80bb8eea3
Thisshon Robert <thisshonrobert0205@gmail.com>
2026-01-17 00:38:28 +0530

    done

---
+++ packages/client/src/components/ChatBot.tsx
@@ -0,0 +1,108 @@
+import { Button } from './ui/button';
+import { FaArrowUp } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+import { useEffect, useRef, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
+
+type FormData = {
+   prompt: string;
+};
+
+type ResponseData = {
+   message: string;
+};
+
+type Message = {
+   role: 'user' | 'assistant';
+   content: string;
+};
+
+const ChatBot = () => {
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
+   const [message, setMessage] = useState<Message[]>([]);
+   const [botTyping, setBotTyping] = useState(false);
+   const [error, setError] = useState<string>();
+   const lastMessageRef = useRef<HTMLDivElement | null>(null);
+   const conversationId = useRef(crypto.randomUUID());
+
+   useEffect(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+   }, [message]);
+
+   const onSubmit = async ({ prompt }: FormData) => {
+      try {
+         setMessage((prev) => [...prev, { role: 'user', content: prompt }]);
+         setBotTyping(true);
+         setError('');
+         reset({ prompt: '' });
+         const { data } = await axios.post<ResponseData>('/api/chat', {
+            prompt,
+            conversationId: conversationId.current,
+         });
+         setMessage((prev) => [
+            ...prev,
+            { role: 'assistant', content: data.message },
+         ]);
+      } catch (error) {
+         console.error(error);
+         setError('Something went wrong..!');
+      } finally {
+         setBotTyping(false);
+      }
+   };
+
+   return (
+      <div className="flex flex-col h-full">
+         <div className="flex flex-col mb-10 gap-2 flex-1">
+            {message.map((item, index) => (
+               <div
+                  key={index}
+                  ref={index === message.length - 1 ? lastMessageRef : null}
+                  className={`px-3 py-2 rounded-3xl 
+                    ${item.role === 'user' ? 'bg-blue-500 text-white self-end' : 'bg-gray-200 text-black self-start'}`}
+               >
+                  <ReactMarkdown>{item.content}</ReactMarkdown>
+               </div>
+            ))}
+            {botTyping && (
+               <div className="flex items-center gap-1 px-4 py-3 bg-gray-100 rounded-2xl self-start">
+                  <div className="w-2 h-2 rounded-full bg-gray-400 animate-bounce-custom [animation-delay:0ms]"></div>
+                  <div className="w-2 h-2 rounded-full bg-gray-400 animate-bounce-custom [animation-delay:200ms]"></div>
+                  <div className="w-2 h-2 rounded-full bg-gray-400 animate-bounce-custom [animation-delay:400ms]"></div>
+               </div>
+            )}
+            {error && <div className="text-red-500 text-sm">{error}</div>}
+         </div>
+         <form
+            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+            onSubmit={handleSubmit(onSubmit)}
+            onKeyDown={(e) => {
+               if (e.key === 'Enter' && !e.shiftKey) {
+                  e.preventDefault();
+                  handleSubmit(onSubmit)();
+               }
+            }}
+         >
+            <textarea
+               {...register('prompt', {
+                  required: true,
+                  validate: (data) => data.trim().length > 0,
+               })}
+               autoFocus
+               className="w-full border-0 focus:outline-0 resize-none"
+               placeholder="Ask anything"
+               maxLength={1000}
+            />
+            <Button
+               disabled={!formState.isValid}
+               className="rounded-full w-9 h-9"
+            >
+               <FaArrowUp />
+            </Button>
+         </form>
+      </div>
+   );
+};
+
+export default ChatBot;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello via Bun!");
+import express from 'express';
+import dotenv from 'dotenv';
+import { type Request, type Response } from 'express';
+import { GoogleGenAI } from '@google/genai';
+import z from 'zod';
+
+dotenv.config();
+
+const PORT = 3000;
+
+const app = express();
+app.use(express.json());
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+const schema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'prompt required')
+      .max(1000, 'max character is 1000'),
+   conversationId: z.uuid(),
+});
+
+const conversations = new Map<string, any[]>();
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const safeParesed = schema.safeParse(req.body);
+
+   if (!safeParesed.success) {
+      res.status(400).json(safeParesed.error.format());
+      return;
+   }
+
+   const { prompt, conversationId } = safeParesed.data;
+
+   // 1. Get history or start a new one
+   const history = conversations.get(conversationId) || [];
+
+   // 2. Add the new user prompt to the history
+   const updatedHistory = [
+      ...history,
+      { role: 'user', parts: [{ text: prompt }] },
+   ];
+
+   try {
+      const airesponse = await ai.models.generateContent({
+         model: 'gemini-2.5-flash',
+         contents: updatedHistory,
+         config: {
+            temperature: 0.2,
+            maxOutputTokens: 200,
+         },
+      });
+
+      const botMessage = { role: 'model', parts: [{ text: airesponse.text }] };
+      conversations.set(conversationId, [...updatedHistory, botMessage]);
+
+      res.json({ message: airesponse.text });
+   } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Generation failed' });
+   }
+});
+
+app.listen(PORT, () => {
+   console.log('lISTENING ON PORT 3000');
+});
